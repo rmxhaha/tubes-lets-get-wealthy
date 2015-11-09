fd44d7d7418564099f372936b8dbbbf220add9b3
Verisky135 <veriskymj@yahoo.co.id>
2015-11-09 19:39:08 +0700

    add int money, chance

--- Player.h
+++ Player.h
@@ -2,12 +2,23 @@
 #define PLAYER_H
 
 // jika bank maka Player *player adalah NULL
+typedef enum {
+	GOTO_PAJAK,
+	GOTO_PENJARA,
+	GOTO_START,
+	GOTO_KELILING_DUNIA,
+	BEBAS_PENJARA,
+	MATI_LAMPU,
+	PERLINDUNGAN,
+	BEBAS_SEWA
+} Chance;
 
 typedef struct TPlayer *PlayerAddress;
 typedef struct TPlayer {
 	char name[5];
 	int money;
 	int revolution_count;
+	Chance save_chance;
 	BlockAddress cblock;
 	PlayerAddress next;
 } Player;
