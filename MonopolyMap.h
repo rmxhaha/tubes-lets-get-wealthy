be95305399860de0a89fba47e0b2d2d1ba1f2559
rmxhaha <code000123@gmail.com>
2015-11-12 12:23:14 +0700

    monopoly map ListTurn and ListOffered

--- MonopolyMap.h
+++ MonopolyMap.h
@@ -10,6 +10,8 @@ typedef struct TPlayer *PlayerAddress;
 
 typedef struct {
 	BlockAddress first;
+	List ListTurn; // giliran
+	List ListOffered; // list barang lelang
 } MonopolyMap;
 
 MonopolyMap load_map(FILE*);
