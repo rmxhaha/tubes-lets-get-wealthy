3ee8c4a898dff0750e8924a7221250452e3c8061
rmxhaha <code000123@gmail.com>
2015-11-12 12:05:27 +0700

    adding spec

--- gamelogic.h
+++ gamelogic.h
@@ -5,6 +5,14 @@
 #include "Player.h"
 #include "MonopolyMap.h"
 
+/**
+	Spec :
+		kalau di nyampe ujung map 
+		- mulai dari map.first lagi 
+		kalau melewati start maka  
+		- revolution_count di tambah
+		- jika revolution_count != 1 maka duit di tambah 150k
+*/
 void pindahPlayer(MonopolyMap, Player*, int d );
 
 
