4ca9365115cf875495b7331cb920cd8994108c57
monstergelo <bimawansatrianto@gmail.com>
2015-11-12 12:29:40 +0700

    Update Player.c

--- Player.c
+++ Player.c
@@ -1,5 +1,15 @@
 #include "Player.h"
 
-void player_init( PlayerAddress );
+void player_init(PlayerAddress Padd)
+{
+    (*Padd).name[0] = '\0';
+    (*Padd).money = 0;
+    (*Padd).revolution_count = 0;
+    (*Padd).save_chance = 0;
+}
 
-void player_alokasi( PlayerAddress );
+void player_alokasi(PlayerAddress Padd)
+{
+    Padd = (Player*) malloc(sizeof(Player));
+}
+
