0ee51cdf14bd464b764970ab83a3ffcede0cc1b1
rmxhaha <code000123@gmail.com>
2015-11-13 22:11:20 +0700

    fix header include

--- Chance.c
+++ Chance.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
-#include <Chance.h>
+#include "Chance.h"
 
-Chance get_chance() { 
+Chance get_chance() {
 	//Memilih kartu chance secara random
 	int x;
-	
+
 	x = rand()%11;
 	return x;
 }
