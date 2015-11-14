59dd2cc9914878bd42c61079bd445bf7d6ac5e64
rmxhaha <code000123@gmail.com>
2015-11-14 12:06:39 +0700

    adding loop for input command

--- game.c
+++ game.c
@@ -3,18 +3,59 @@
 #include "MonopolyMap.h"
 #include "infopetak.h"
 
+#define ifCommand(cmd)\
+	if( strcmp( command, cmd) == 0 )
+
 int main(){
 	MonopolyMap map;
 	Block block;
 	Address cplayer; // current player
+	char command[100];
 
 	FILE *f = fopen("mapdata.txt","r");
     map = load_map(f);
     fclose(f);
 
-    pick_jumlah_player(&map);
+    pick_jumlah_player(map);
 	cplayer = First(map.ListTurn);
-	
+
+	// repeat until game is won
+	do {
+		scanf("%s", command );
+		ifCommand("rolldice"){
+
+		}
+		else ifCommand("info"){
+
+		}
+		else ifCommand("buy"){
+
+		}
+		else ifCommand("sell"){
+
+		}
+		else ifCommand("sellbank"){
+
+		}
+		else ifCommand("showoffered"){
+
+		}
+		else ifCommand("upgrade"){
+
+		}
+		else ifCommand("board"){
+
+		}
+		else ifCommand("leaderboard"){
+
+		}
+		else {
+			printf("perintah tidak diketahui\n");
+		}
+
+	} while( true );
+
+
     BlockAddress addr = map.first;
     addr = map.first->map_next;
 
