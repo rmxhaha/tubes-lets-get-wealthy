e9fde34a7e8ead4ca9d73e8a6da655b4e22d7b7e
friskanat <friskanataniel@gmail.com>
2015-11-18 13:15:22 +0700

    update pindah_player_1 + travel

--- worldtravel.c
+++ worldtravel.c
@@ -7,11 +7,11 @@ void player_travel(MonopolyMap map, PlayerAddress player,char* namatempat)
     b = search_block_by_name(map,namatempat);
     if(b!=Nil)
     {
-       pindah_player_ke(&map, player,b);
+       pindah_player_ke(map, player,b);
        printf("Selamat! Sekarang anda berada di %s\n",b->name);
     }
     else
     {
-        printf("anda tidak bisa ke sana\n"); /*di deskripsi soal tulisannya nama kota, jd asumsi gaboleh ke start chance lalala cuma bole ke rekreasi / kota.*/
+        printf("anda tidak bisa ke sana\n");
     }
 }
